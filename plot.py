3ce9de7559fb9db298469b8c427d01171cb7ca66
Михаил Исаков <maisakov@miem.hse.ru>
2021-03-24 15:45:10 +0300

    Costomize graph. X axe scale changed to month-year

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ def form_plot():
     fig = go.Figure()
     zulip_data = get_zulip_data('Павел Блинов')[0]
     git_data = get_git_data('Павел Блинов')[0]
-    jitsiClasses_data = get_jitsiClasses_data('pdblinov@miem.hse.ru')[0]
+    jitsiClasses_data = get_jitsiClasses_data('maisakov@miem.hse.ru')[0]
     fig.add_trace(go.Bar(name = 'Zulip Activity', x = list(zulip_data.keys()), y = list(zulip_data.values()),text=list(zulip_data.values()), textposition='auto'))
     
     fig.add_trace(go.Bar(name = 'GitLab Activity', x = list(git_data.keys()), y = list(git_data.values()),text=list(git_data.values()), textposition='auto'))
@@ -19,6 +19,9 @@ def form_plot():
                     yaxis_title='Amount',
                     barmode = 'group'
                     )
+    fig.update_xaxes(
+    dtick="M1",
+    tickformat="%b-%Y")
     #plotly.offline.plot(fig, include_plotlyjs=False, output_type='div')
     fig.show()
 
